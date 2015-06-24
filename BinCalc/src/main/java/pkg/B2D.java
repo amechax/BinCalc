42edc928738a72028154ea5c630ede1b4e7aa198
Rhinoxo <justin@orner.net>
2015-06-24 16:54:01 -0700

    Added first version of operations. Need to test.

--- BinCalc/src/main/java/pkg/B2D.java
+++ BinCalc/src/main/java/pkg/B2D.java
@@ -1,12 +1,20 @@
 package pkg;
 // added skeleton methods for the class
 public class B2D {
-	public static void main(String[] bin) {
+	public static void main(long bin) {
+
 		System.out.println(bin);
+		expandBinary();
+		System.out.println(dec);
 	}
 	
+	// Expands the Binary input into a Decimal output
 	public void expandBinary()
 	{
-		
+		int i = 0;
+		long dec;
+		while (i != bin.len()) {
+			dec += bin[bin.len() - i] * (2 ** i);
+		}
 	}
 }
